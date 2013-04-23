88511775b31f32412a30b116c21fa6a072371e98
johnedvard <johnedvard89@gmail.com>
2013-04-23 16:46:22 +0200

    container

---
+++ SpleiseKalkulator/DataContainer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using Microsoft.Phone.UserData;
+namespace SpleiseKalkulator
+{
+    class DataContainer
+    {
+
+       public static Contact contact = null;
+    }
+}
